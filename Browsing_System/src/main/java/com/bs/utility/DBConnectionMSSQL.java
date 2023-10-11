bd3cb908e9caa4c64bec317a90537334436ded26
Teran Sarathchandra <Teran.Sarathchandra@softlogiclife.lk>
2023-10-12 00:30:43 +0530

    All users Database connections added

--- Browsing_System/src/main/java/com/bs/utility/DBConnectionMSSQL.java
+++ Browsing_System/src/main/java/com/bs/utility/DBConnectionMSSQL.java
@@ -13,9 +13,20 @@ public class DBConnectionMSSQL {
 
 		try {
 			
+//			Teran
 			String dbURL = "jdbc:sqlserver://localhost;DatabaseName=OnlineWatchingMoviesandTVSeries;encrypt=true;trustServerCertificate=true;";
 			String userName = "sa";
 			String password = "Pass@1234#";
+			
+//			Panchali
+//			String dbURL = "jdbc:sqlserver://DESKTOP-8G691HC\\SQLEXPRESS;DatabaseName=OnlineWatchingMoviesandTVSeries;encrypt=true;trustServerCertificate=true;";
+//			String userName = "sa";
+//			String password = "*272617Ps*";
+			
+//			Yethmi
+//			dbURL = "jdbc:sqlserver://YETHMI-PC\\SQLEXPRESS;DatabaseName=OnlineWatchingMoviesandTVSeries;encrypt=true;trustServerCertificate=true;";
+//			userName = "sa";
+//			password = "yethmi";
 			
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			
