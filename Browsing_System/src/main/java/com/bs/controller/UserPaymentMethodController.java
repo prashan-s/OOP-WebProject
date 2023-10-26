ee19d3acea3e897477b083e6bdcd08b88a2e97cc
Yethmi Gamage <yethmigamage@gmail.com>
2023-10-27 03:15:55 +0530

    Admin portal - Manage User completed

--- Browsing_System/src/main/java/com/bs/controller/UserPaymentMethodController.java
+++ Browsing_System/src/main/java/com/bs/controller/UserPaymentMethodController.java
@@ -26,12 +26,11 @@ public class UserPaymentMethodController {
 	PaymentDAO paymentDao;
 	UserDAO userDao;
 	java.util.Date exp = new java.util.Date();
-	
+
 	public UserPaymentMethodController() {
-		
+
 	}
-	
-	
+
 	public UserPaymentMethodController(HttpServletRequest request, HttpServletResponse response) {
 		this.request = request;
 		this.response = response;
@@ -40,162 +39,160 @@ public class UserPaymentMethodController {
 		this.paymentDao = new PaymentDAO();
 		this.userDao = new UserDAO();
 	}
+
 	public void doAction(String action) {
-		
+
 		String jspPage = "UserPaymentMethod.jsp";
 		int userId = -1;
 		boolean showUserIdForm = false;
 		boolean listPaymentMethod = false;
-		boolean addPaymentMethod =  false;
-		boolean showDeleteStatus=  false;
+		boolean addPaymentMethod = false;
+		boolean showDeleteStatus = false;
 		boolean showInsertStatus = false;
 		boolean showPaymentStatus = false;
-		switch(action) {
-	
-	case "submit":
-	
-		listPaymentMethod = true;
-		addPaymentMethod = true;
-		
-		jspPage = "UserPaymentMethod.jsp";
-		userId = Integer.parseInt(this.request.getParameter("userId"));
-		request.setAttribute("methods", this.selectPaymentMethod(userId));
+		switch (action) {
 
-		Cookie cookie = new Cookie("userId", Integer.toString(userId));
-		this.response.addCookie(cookie);
-		break;
-		
-	case "pay":
-		jspPage = "UserPaymentMethod.jsp";
-		showPaymentStatus = true;
-		String paymentMessage = "";
-		String premiumMessage = "";
-		
-		userId = Integer.parseInt(request.getParameter("userId"));
-		UserSubscription userSub = new UserSubscription();
-		userSub.setUserId(userId);
-		userSub.setPlanId(Integer.parseInt(request.getParameter("planId")));
-		int subId = userSubDao.insertSubReturnSubId(userSub);
-		
-		Payment payment = new Payment();
-		payment.setUserId(userId);
-		payment.setAmount(Float.parseFloat(request.getParameter("amount")));
-		payment.setSubId(subId);
-		Boolean paymentStatus = paymentDao.insertPayment(payment);
-		
-		System.out.println(paymentStatus+ "in pay tag");
-		
-		if(paymentStatus == false) {
-			paymentMessage = "Payment failed..";
-		}else {
-			paymentMessage = "Payment Success..";
-			boolean premiumStatus = userDao.upgradeToPremium(userId);
-			if(premiumStatus == true) {
-				premiumMessage = "You are now Premium Member";
-			}else {
-				premiumMessage = "Still not a Premium Member";
+		case "submit":
+
+			listPaymentMethod = true;
+			addPaymentMethod = true;
+
+			jspPage = "UserPaymentMethod.jsp";
+			userId = Integer.parseInt(this.request.getParameter("userId"));
+			request.setAttribute("methods", this.selectPaymentMethod(userId));
+
+			Cookie cookie = new Cookie("userId", Integer.toString(userId));
+			this.response.addCookie(cookie);
+			break;
+
+		case "pay":
+			jspPage = "UserPaymentMethod.jsp";
+			showPaymentStatus = true;
+			String paymentMessage = "";
+			String premiumMessage = "";
+
+			userId = Integer.parseInt(request.getParameter("userId"));
+			UserSubscription userSub = new UserSubscription();
+			userSub.setUserId(userId);
+			userSub.setPlanId(Integer.parseInt(request.getParameter("planId")));
+			int subId = userSubDao.insertSubReturnSubId(userSub);
+
+			Payment payment = new Payment();
+			payment.setUserId(userId);
+			payment.setAmount(Float.parseFloat(request.getParameter("amount")));
+			payment.setSubId(subId);
+			Boolean paymentStatus = paymentDao.insertPayment(payment);
+
+			System.out.println(paymentStatus + "in pay tag");
+
+			if (paymentStatus == false) {
+				paymentMessage = "Payment failed..";
+			} else {
+				paymentMessage = "Payment Success..";
+				boolean premiumStatus = userDao.upgradeToPremium(userId);
+				if (premiumStatus == true) {
+					premiumMessage = "You are now Premium Member";
+				} else {
+					premiumMessage = "Still not a Premium Member";
+				}
 			}
-		}
-		request.setAttribute("paymentMessage", paymentMessage);
-		request.setAttribute("premiumMessage", premiumMessage);
-		break;
-		
-	case "remove":
-		jspPage = "UserPaymentMethod.jsp";
-		showDeleteStatus = true;
-		boolean deleteStatus = false;
-		String deleteMessage = null;
+			request.setAttribute("paymentMessage", paymentMessage);
+			request.setAttribute("premiumMessage", premiumMessage);
+			break;
 
-		int paymentMethodId = Integer.parseInt(this.request.getParameter("paymentMethodId"));
+		case "remove":
+			jspPage = "UserPaymentMethod.jsp";
+			showDeleteStatus = true;
+			boolean deleteStatus = false;
+			String deleteMessage = null;
 
-		deleteStatus = this.deletePaymentMethod(paymentMethodId);
-		System.out.println(deleteStatus + "do action");
-		if (deleteStatus == false) {
-			deleteMessage = "failed to remove";
-		} else {
-			deleteMessage = "removed from Payment Methods";
-		}
-		request.setAttribute("deleteMessage", deleteMessage);
-		break;
-	
-	case "add":
-		jspPage = "UserPaymentMethod.jsp";
-		showInsertStatus = true;
-		boolean insertStatus = false;
-		String insertMessage = null;
-		
-		UserPaymentMethod method = new UserPaymentMethod();
-		SimpleDateFormat dateFormatPayMethod = new SimpleDateFormat("yyyy-MM-dd");
-		
-		method.setCardNumber(request.getParameter("cardNo"));
-		try {
-			exp = dateFormatPayMethod.parse(request.getParameter("expDate"));
-			java.sql.Date expMethod = new java.sql.Date(exp.getTime());
+			int paymentMethodId = Integer.parseInt(this.request.getParameter("paymentMethodId"));
+
+			deleteStatus = this.deletePaymentMethod(paymentMethodId);
+			if (deleteStatus == false) {
+				deleteMessage = "failed to remove";
+			} else {
+				deleteMessage = "removed from Payment Methods";
+			}
+			request.setAttribute("deleteMessage", deleteMessage);
+			break;
+
+		case "add":
+			jspPage = "UserPaymentMethod.jsp";
+			showInsertStatus = true;
+			boolean insertStatus = false;
+			String insertMessage = null;
+
+			UserPaymentMethod method = new UserPaymentMethod();
+			SimpleDateFormat dateFormatPayMethod = new SimpleDateFormat("yyyy-MM-dd");
+
+			method.setCardNumber(request.getParameter("cardNo"));
+			try {
+				exp = dateFormatPayMethod.parse(request.getParameter("expDate"));
+				java.sql.Date expMethod = new java.sql.Date(exp.getTime());
 				method.setCardExpiryDate(expMethod);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		method.setCvv(Integer.parseInt(request.getParameter("cvv")));
-		method.setUserId(getValueForId("userId"));
-		
-		insertStatus = this.addUserPaymentMethod(method);
-		System.out.println(insertStatus + "insert do action");
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			method.setCvv(Integer.parseInt(request.getParameter("cvv")));
+			method.setUserId(getValueForId("userId"));
 
-		if(insertStatus == false) {
-			insertMessage = "Failed to add payment method..";
-			
-		}else {
-			insertMessage =  "Successfully add payment Method..";
-		}
-		request.setAttribute("insertMessage", insertMessage);
+			insertStatus = this.addUserPaymentMethod(method);
 
-		break;
-		
+			if (insertStatus == false) {
+				insertMessage = "Failed to add payment method..";
+
+			} else {
+				insertMessage = "Successfully add payment Method..";
+			}
+			request.setAttribute("insertMessage", insertMessage);
+
+			break;
+
 		}
 		try {
-		request.setAttribute("showUserIdForm", showUserIdForm);
-		request.setAttribute("listPaymentMethod", listPaymentMethod);
-		request.setAttribute("addPaymentMethod", addPaymentMethod);
-		request.setAttribute("showDeleteStatus", showDeleteStatus);
-		request.setAttribute("showInsertStatus", showInsertStatus);
-		request.setAttribute("showPaymentStatus", showPaymentStatus);
-		
-		this.dispacther = request.getRequestDispatcher(jspPage);
-		dispacther.forward(request, response);
+			request.setAttribute("showUserIdForm", showUserIdForm);
+			request.setAttribute("listPaymentMethod", listPaymentMethod);
+			request.setAttribute("addPaymentMethod", addPaymentMethod);
+			request.setAttribute("showDeleteStatus", showDeleteStatus);
+			request.setAttribute("showInsertStatus", showInsertStatus);
+			request.setAttribute("showPaymentStatus", showPaymentStatus);
 
-	} catch (Exception e2) {
-		e2.printStackTrace();
+			this.dispacther = request.getRequestDispatcher(jspPage);
+			dispacther.forward(request, response);
+
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
 	}
-	}
-	public List<UserPaymentMethod>  selectPaymentMethod(int userId) {
-		
+
+	public List<UserPaymentMethod> selectPaymentMethod(int userId) {
+
 		List<UserPaymentMethod> methods = null;
 		try {
 			methods = this.dao.selectUserPaymentMethod(userId);
-		//	request.setAttribute("methods", methods);
-		}catch(Exception e1){
+		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
-		
+
 		return methods;
 	}
-	
-	public boolean  deletePaymentMethod(int paymentMethodId) {
-		
+
+	public boolean deletePaymentMethod(int paymentMethodId) {
+
 		boolean deleteStatus = false;
 		try {
 			deleteStatus = dao.deleteUserPaymentMethod(paymentMethodId);
-			System.out.println(deleteStatus + "ctrl method");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return deleteStatus;
 	}
+
 	public boolean addUserPaymentMethod(UserPaymentMethod userPaymentMethod) {
 		boolean insertStatus = false;
 		try {
 			insertStatus = dao.insertUserPaymentMethod(userPaymentMethod);
-			System.out.println(insertStatus + "insert ctrl metho");
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -203,7 +200,6 @@ public class UserPaymentMethodController {
 		return insertStatus;
 	}
 
-	
 	private int getValueForId(String key) {
 		Cookie[] cookies = null;
 		cookies = request.getCookies();
--- Browsing_System/src/main/java/com/bs/dao/UserDAO.java
+++ Browsing_System/src/main/java/com/bs/dao/UserDAO.java
@@ -5,6 +5,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.List;
 
 import com.bs.interfaces.IUserDAO;
 import com.bs.model.User;
@@ -14,20 +15,28 @@ public class UserDAO implements IUserDAO {
 
 	private static final String SELECT_USER_BY_ID = "SELECT user_id, name, email, mobile_no, dob , premium_user, password , "
 			+ "created_admin_name, is_active, row_created_datetime " + "FROM users " + "WHERE user_id = ? ";
+	// admin select user list
+	private static final String SELECT_USER_LIST = "SELECT user_id, name, email, mobile_no, dob , premium_user, password , "
+			+ "created_admin_name, is_active, row_created_datetime " + "FROM users "
+			+ "ORDER BY row_created_datetime DESC ";
+	// when login
+	private static final String SELECT_PASSWORD_BY_USERNAME = "SELECT password " + "FROM users " + "WHERE name = ?  ";
+
 	// when user sign up
 	private static final String INSERT_USER = "INSERT INTO users (name, email, mobile_no , dob , password )"
 			+ "VALUES (?, ? , ? , ? , ? )";
 	// when user update details
 	private static final String UPDATE_USER_BY_USER = "UPDATE users SET name = ? , email =  ? , mobile_no = ? , dob = ?  "
 			+ "WHERE user_id = ?";
+	// when admin update details
+	private static final String UPDATE_USER_BY_ADMIN = "UPDATE users SET name = ? , email =  ? , mobile_no = ? , dob = ? , premium_user = ? "
+			+ "WHERE user_id = ?";
 	// when user upgrade to premium
 	private static final String UPGRADE_TO_PREMIUM = "UPDATE users SET premium_user = 1 " + "WHERE user_id = ? ";
 	// when user change password
 	private static final String UPDATE_PASSWORD = "UPDATE users SET password = ? " + "WHERE user_id = ?";
-
+	// when user or admin delete user
 	private static final String DELETE_USER = "DELETE FROM users " + "WHERE user_id = ? ";
-
-	private static final String SELECT_PASSWORD_BY_USERNAME = "SELECT password " + "FROM users " + "WHERE name = ?  ";
 
 	public User selectUser(int userId) {
 
@@ -62,6 +71,41 @@ public class UserDAO implements IUserDAO {
 
 	}
 
+	// all user list
+	public List<User> selectUserList() {
+
+		List<User> users = new ArrayList<>();
+		try {
+			Connection con = DBConnectionMSSQL.getConnection();
+			PreparedStatement stmt = con.prepareStatement(SELECT_USER_LIST);
+
+			ResultSet rs = stmt.executeQuery();
+			
+			while (rs.next()) {
+				int returnedUserId = rs.getInt("user_id");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				String mobileNo = rs.getString("mobile_no");
+				Date dob = rs.getDate("dob");
+				boolean premium_user = rs.getBoolean("premium_user");
+				String password = rs.getString("password");
+				String created_admin_name = rs.getString("created_admin_name");
+				boolean is_active = rs.getBoolean("is_active");
+				Date row_created_datetime = rs.getDate("row_created_datetime");
+
+				User user = new User(returnedUserId, name, email, mobileNo, dob, premium_user, password, created_admin_name,
+						is_active, row_created_datetime);
+				users.add(user);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return users;
+
+	}
+
+	// user login
 	public String loginUser(String name) {
 		String password = null;
 		try {
@@ -83,6 +127,7 @@ public class UserDAO implements IUserDAO {
 
 	}
 
+	// user signup
 	public boolean insertUser(User user) {
 
 		boolean rowInserted = false;
@@ -104,6 +149,7 @@ public class UserDAO implements IUserDAO {
 		return rowInserted;
 	}
 
+	// user edit
 	public boolean updateUserByUser(User user) {
 
 		boolean rowUpdated = false;
@@ -128,6 +174,32 @@ public class UserDAO implements IUserDAO {
 		return rowUpdated;
 	}
 
+	// admin edit
+	public boolean updateUserByAdmin(User user) {
+
+		boolean rowUpdated = false;
+
+		try {
+			Connection con = DBConnectionMSSQL.getConnection();
+			PreparedStatement stmt = con.prepareStatement(UPDATE_USER_BY_ADMIN);
+
+			stmt.setString(1, user.getName());
+			stmt.setString(2, user.getEmail());
+			stmt.setString(3, user.getMobileNo());
+			Date dob = new Date(user.getDob().getTime());
+			stmt.setDate(4, dob);
+			stmt.setBoolean(5, user.isPremiumUser());
+
+			stmt.setInt(6, user.getUserId());
+
+			rowUpdated = (stmt.executeUpdate() > 0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("user dao 119 " + rowUpdated);
+		return rowUpdated;
+	}
+
 	// upgrade to premium
 	public boolean upgradeToPremium(int userId) {
 
@@ -140,9 +212,9 @@ public class UserDAO implements IUserDAO {
 			stmt.setInt(1, userId);
 
 			rowUpdated = (stmt.executeUpdate() > 0);
-			
+
 			System.out.println(rowUpdated + "in user dao premium");
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -182,7 +254,7 @@ public class UserDAO implements IUserDAO {
 			Connection con = DBConnectionMSSQL.getConnection();
 			PreparedStatement stmt = con.prepareStatement(DELETE_USER);
 
-			stmt.setInt(2, userID);
+			stmt.setInt(1, userID);
 			rowDelete = (stmt.executeUpdate() > 0);
 
 		} catch (Exception e) {
